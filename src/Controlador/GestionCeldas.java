5eef92b26548953675ff449cf7faef926358be25
KevinZambran <kzambrano8@utmachala.edu.ec>
2021-08-13 11:48:11 -0500

    Gestion de Celdas y encabezados de un Jtable

--- src/Controlador/GestionCeldas.java
+++ src/Controlador/GestionCeldas.java
@@ -3,7 +3,6 @@ package Controlador;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
-import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
